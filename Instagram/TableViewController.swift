7329bc542270d345375a6852e09cf65ff9ccd0ef
Khan <ycchencoding@gmail.com>
2015-10-14 17:50:42 -0400

    add pull down refresh feature

--- Instagram/TableViewController.swift
+++ Instagram/TableViewController.swift
@@ -16,9 +16,76 @@ class TableViewController: UITableViewController {
     var userids = [String]()
     var isFollowing = [String : Bool]()
     
+    var refresher: UIRefreshControl!
+    
+    func refresh(){
+        
+        let query = PFUser.query()
+        
+        query?.findObjectsInBackgroundWithBlock({ (objects, error) -> Void in
+            
+            if let users = objects {
+                
+                self.usernames.removeAll(keepCapacity: true)
+                self.userids.removeAll(keepCapacity: true)
+                self.isFollowing.removeAll(keepCapacity: true)
+                
+                for object in users{
+                    if let user = object as? PFUser {
+                        
+                        if user.objectId! != PFUser.currentUser()!.objectId{
+                            self.usernames.append(user.username!)
+                            self.userids.append(user.objectId!)
+                            
+                            let query = PFQuery(className: "followers")
+                            
+                            query.whereKey("follower", equalTo: (PFUser.currentUser()?.objectId)!)
+                            query.whereKey("following", equalTo: user.objectId!)
+                            
+                            query.findObjectsInBackgroundWithBlock({ (objects, error) -> Void in
+                                
+                                if let objects = objects {
+                                    
+                                    
+                                    if objects.count > 0 {
+                                        
+                                        
+                                        self.isFollowing[user.objectId!] = true
+                                        
+                                    } else{
+                                        self.isFollowing[user.objectId!] = false
+                                    }
+                                }
+                                if self.isFollowing.count == self.usernames.count {
+                                    
+                                    self.tableView.reloadData()
+                                    self.refresher.endRefreshing()
+                                }
+                            })
+                        }
+                        
+                    }
+                }
+            }
+            
+            
+        })
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        refresher = UIRefreshControl()
+        
+        refresher.attributedTitle = NSAttributedString(string: "pull to refresh")
+        
+        refresher.addTarget(self, action: "refresh", forControlEvents: UIControlEvents.ValueChanged)
+        
+        self.tableView.addSubview(refresher)
+        
+        refresh()
+        
         let query = PFUser.query()
         
         query?.findObjectsInBackgroundWithBlock({ (objects, error) -> Void in
